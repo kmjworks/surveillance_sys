fa596fa263ccef6fbebae74866876ff75021bfaf
Karl Marten Jaaska <kajaas@taltech.ee>
2025-02-23 05:48:46 +0200

    clang configuration, .vscode config

--- src/nodes/motion_detector_node.cpp
+++ src/nodes/motion_detector_node.cpp
@@ -2,16 +2,22 @@
 #include <sensor_msgs/Image.h>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
+#include <surveillance_system/MotionEvent.h>
+#include <image_transport/image_transport.h>
 #include <vector>
 
 class MotionDetectorNode
 {
     private:
         ros::NodeHandle nh_;
+        image_transport::ImageTransport it_;
         ros::Subscriber image_sub_;
+        ros::Publisher event_pub_;
+        image_transport::Publisher debug_pub_;
         cv::Mat previous_frame_;
         bool first_frame_;
         double detection_threshold_;
+        uint32_t event_counter_;
 
     public:
         MotionDetectorNode() : nh_("~"), first_frame_(true)
@@ -21,6 +27,11 @@ class MotionDetectorNode
             image_sub_ = nh_.subscribe("/camera_node/image_raw", 1,
             &MotionDetectorNode::imageCallback, this);
 
+            event_pub_ = nh_.advertise<surveillance_system::MotionEvent>
+                ("motion_events", 10);
+            
+            debug_pub_ = it_.advertise("debug_image", 1);
+
             ROS_INFO("Motion detector initalized.");
         }
 
@@ -29,9 +40,11 @@ class MotionDetectorNode
         {
             try {
                 cv::Mat current_frame = cv::bridge::toCvShare(msg, "bgr8")->image;
+                cv::Mat debug_frame == curret_frame.clone();
+                cv::Mat current_gray;
+
 
                 // grayscale conversion
-                cv::Mat current_gray;
                 cv::cvtColor(current_frame, current_gray, cv::COLOR_BGR2GRAY);
 
                 if (first_frame_) {
@@ -58,8 +71,14 @@ class MotionDetectorNode
                     if(cv::contourArea(contour) > 100)
                     {
                         cv::Rect bbox = cv::boundingRect(contour);
-                        ROS_INFO("Motion detected at x: %d, y: %d, width: %d, height: %d",
-                            bbox.x, bbox.y, bbox.width, bbox.height);
+                        cv::Moments moments = cv::moments(contour);
+                        
+                        cv::rectangle(debug_frame, bbox, cv::Scalar(0, 255, 0), 2);
+
+                        surveillance_system::MotionEvent event; 
+
+                        event.header = msg->header;
+                        event.event_id = ++event_counter_;
                     }
                 }
 
